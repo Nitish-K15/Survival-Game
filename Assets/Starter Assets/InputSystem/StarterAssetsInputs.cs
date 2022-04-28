c5053ba12783f9940a4977724d35f93cf077c920
Nitish-K15 <79478893+Nitish-K15@users.noreply.github.com>
2022-04-28 20:26:46 +0530

    Initial Commit

---
+++ Assets/Starter Assets/InputSystem/StarterAssetsInputs.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+using UnityEngine.InputSystem;
+#endif
+
+namespace StarterAssets
+{
+	public class StarterAssetsInputs : MonoBehaviour
+	{
+		[Header("Character Input Values")]
+		public Vector2 move;
+		public Vector2 look;
+		public bool jump;
+		public bool sprint;
+
+		[Header("Movement Settings")]
+		public bool analogMovement;
+
+#if !UNITY_IOS || !UNITY_ANDROID
+		[Header("Mouse Cursor Settings")]
+		public bool cursorLocked = true;
+		public bool cursorInputForLook = true;
+#endif
+
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+		public void OnMove(InputAction.CallbackContext value)
+		{
+			MoveInput(value.ReadValue<Vector2>());
+		}
+
+		public void OnLook(InputAction.CallbackContext value)
+		{
+			if (cursorInputForLook)
+			{
+				LookInput(value.ReadValue<Vector2>());
+			}
+		}
+
+		public void OnJump(InputAction.CallbackContext value)
+		{
+			JumpInput(value.action.triggered);
+		}
+
+		public void OnSprint(InputAction.CallbackContext value)
+		{
+			SprintInput(value.action.ReadValue<float>() == 1);
+		}
+#else
+	// old input sys if we do decide to have it (most likely wont)...
+#endif
+
+
+		public void MoveInput(Vector2 newMoveDirection)
+		{
+			move = newMoveDirection;
+		} 
+
+		public void LookInput(Vector2 newLookDirection)
+		{
+			look = newLookDirection;
+		}
+
+		public void JumpInput(bool newJumpState)
+		{
+			jump = newJumpState;
+		}
+
+		public void SprintInput(bool newSprintState)
+		{
+			sprint = newSprintState;
+		}
+
+#if !UNITY_IOS || !UNITY_ANDROID
+
+		private void OnApplicationFocus(bool hasFocus)
+		{
+			SetCursorState(cursorLocked);
+		}
+
+		private void SetCursorState(bool newState)
+		{
+			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+		}
+
+#endif
+
+	}
+	
+}
